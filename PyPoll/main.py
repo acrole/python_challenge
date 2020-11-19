5856c547b0841400ca5477f8d07a732fe309b90b
acrole <averycroley@gmail.com>
2020-11-18 18:09:18 -0600

    first_commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,10 @@
+import os
 
+csvpath = os.path.join('Resources', 'election_data')
+
+import csv
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter='')
+
+print(csvreader)
